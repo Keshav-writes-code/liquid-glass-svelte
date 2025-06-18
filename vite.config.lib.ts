094568b411a5596e7ae462607a94024a00bd43bf
xieziqian <xieziqian@siluzan.com>
2025-06-18 19:12:52 +0800

    update:处理样式冲突

--- vite.config.lib.ts
+++ vite.config.lib.ts
@@ -3,13 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-
+import tailwindcss from '@tailwindcss/vite'
 import dts from 'vite-plugin-dts'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    tailwindcss(),
     dts({
       rollupTypes: true,
       include: ['src/lib/**/*'],
